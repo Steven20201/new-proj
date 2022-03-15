b5ba82c05080787574c8026294ec6d402181f21c
Steven20201 <khantzawhein261@example.com>
2022-03-16 06:15:15 +0800

    commit

--- function.js
+++ function.js
@@ -1,34 +1,3 @@
-var slideIndex = [1,1,1,1,1,1];
-var slidesID =["slide-1","slide-2","slide-3","slide-4","slide-5","slide-6"]
-showSlides(1,0);
-showSlides(1,1);
-showSlides(1,2);
-showSlides(1,3);
-showSlides(1,4);
-showSlides(1,5);
-
-function plusSlides(n,no){
-    showSlides(slideIndex[no] += n,no);
-}
-
-function showSlides(n,no) {
-    var i;
-    var x = document.getElementsByClassName(slidesID[no]);
-    if(n>x.length) {
-        slideIndex[no] = 1;
-    }
-
-    if(n<1) {
-        slideIndex[no] = x.length;
-    }
-    
-    for(i = 0; i <x.length ; i++){
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex[no]-1].style.display = "block";
-}
-
 
 
 
